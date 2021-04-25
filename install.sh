8c81e158f6814d895422ae525c470665a4b6ad79
Chandlerzz <Chandlerzz@github.com>
2021-04-25 10:37:43 +0800

    .gitignore

--- install.sh
+++ install.sh
@@ -4,11 +4,11 @@ mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/surround.git
 vim -u NONE -c "helptags surround/doc" -c q
-mkdir -p ~/.vim/pack/tpope/start
+# mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/commentary.git
 vim -u NONE -c "helptags commentary/doc" -c q
-mkdir -p ~/.vim/pack/tpope/start
+# mkdir -p ~/.vim/pack/tpope/start
 cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/fugitive.git
 vim -u NONE -c "helptags fugitive/doc" -c q
